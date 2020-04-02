6a87fad2c6c6269916f7fcb820425d0c9bfb6508
Mouad AITALI <mouaadaitali@icloud.com>
2020-04-02 23:39:20 +0100

    UI improvement, added countries flags, added Google Map with markers

---
+++ app/src/main/java/maa/coronavirustracker_kotlin_vesion/Model/All.kt
@@ -0,0 +1,11 @@
+package maa.coronavirustracker_kotlin_vesion.Model
+
+
+data class All(
+    val cases: Long,
+    val deaths: Long,
+    val recovered: Long,
+    val updated: Long,
+    val active: Long,
+    val affectedCountries: Long
+)
