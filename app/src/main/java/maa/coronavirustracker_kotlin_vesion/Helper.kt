d1f21de0493192c15e9f4437a3bf7bb263d9d792
Mouad AITALI <mouaadaitali@icloud.com>
2020-05-09 02:35:06 +0000

    Upgrade the endpoint to v2, Show more details when clicking on the map mark

---
+++ app/src/main/java/maa/coronavirustracker_kotlin_vesion/Helper.kt
@@ -0,0 +1,31 @@
+package maa.coronavirustracker_kotlin_vesion
+
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.widget.TextView
+
+class Helper {
+    public fun setCustomColor(
+        textView: TextView,
+        label: String,
+        value: String,
+        ColorHex: Int
+    ) {
+        val LabelValue: String = label + value;
+        val spannable: Spannable = SpannableString(LabelValue);
+        spannable.setSpan(
+            ForegroundColorSpan(ColorHex),
+            label.length,
+            (label + value).length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        );
+        spannable.setSpan(
+            android.text.style.StyleSpan(android.graphics.Typeface.BOLD),
+            label.length,
+            (label + value).length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        );
+        textView.setText(spannable, TextView.BufferType.SPANNABLE);
+    }
+}
